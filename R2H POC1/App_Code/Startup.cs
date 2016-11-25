457f8d4f2654f99d9117bc45ac111367a814a6e7
HillJD <john.hillen@gmail.com>
2016-11-25 00:07:16 -0500

    Added POC to prove GitHub

---
+++ R2H POC1/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Road2Harvest.Startup))]
+namespace Road2Harvest
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
