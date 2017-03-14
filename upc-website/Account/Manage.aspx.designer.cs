36a218ca2657c0d00c5d267d6f5c4cb9c9865fb2
Michael <michael@creekviewfarms.net>
2017-03-14 15:10:42 -0400

    Username display on manage accounts page

--- upc-website/Account/Manage.aspx.designer.cs
+++ upc-website/Account/Manage.aspx.designer.cs
@@ -38,14 +38,5 @@ namespace upc_website.Account {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.HyperLink CreatePassword;
-        
-        /// <summary>
-        /// PhoneNumber control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Label PhoneNumber;
     }
 }
