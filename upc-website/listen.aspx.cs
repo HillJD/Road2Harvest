88fe81f8f3f5c130b443e7206b01af42c3612145
Michael <michael@creekviewfarms.net>
2017-09-13 08:48:06 -0400

    Listen Page Content added with testing labels

--- upc-website/listen.aspx.cs
+++ upc-website/listen.aspx.cs
@@ -11,7 +11,51 @@ namespace upc_website
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            bool DayCheck = false;
+            bool TimeCheck = false;
+            String Day = DateTime.Now.DayOfWeek.ToString();
+            String Time =DateTime.Now.TimeOfDay.ToString();
+            TimeSpan SundayStart1 = new TimeSpan(11, 00, 00);
+            TimeSpan SundayStart2 = new TimeSpan(18, 00, 00);
+            TimeSpan SundayEnd1 = new TimeSpan(12, 15, 00);
+            TimeSpan SundayEnd2 = new TimeSpan(19, 30, 00);
+            TimeSpan WednesdayStart = new TimeSpan(19, 30, 00);
+            TimeSpan WednesdayEnd = new TimeSpan(20, 30, 00);
 
+            Label1.Text = Day + " " + Time;
+            if (Day == "Wednesday")
+            {
+                DayCheck = true;
+                if (DateTime.Now.TimeOfDay >= WednesdayStart && DateTime.Now.TimeOfDay <= WednesdayEnd) TimeCheck = true;
+                
+            }
+            else if (Day == "Sunday")
+            {
+                DayCheck = true;
+                if (DateTime.Now.TimeOfDay >= SundayStart1 && DateTime.Now.TimeOfDay <= SundayEnd1) TimeCheck = true;
+                else if (DateTime.Now.TimeOfDay >= SundayStart2 && DateTime.Now.TimeOfDay <= SundayEnd2) TimeCheck = true;
+            }
+
+            
+            if (DayCheck) Label2.Text = "Day Check True"; else Label2.Text = "Day Check False";
+            if (TimeCheck) Label3.Text = "Time Check True"; else Label3.Text = "Time Check False";
+            if (DayCheck && TimeCheck) ImageButton1.ImageUrl = "~/images/live stream.jpg";
+
+        }
+
+        protected void ImageButton1_Click(object sender, ImageClickEventArgs e)
+        {
+            Response.Redirect("http://unionpentecostal.dyndns.org:88/broadwave.mp3?src=1&rate=1&ref=http%3A%2F%2Funionpentecostal.dyndns.org%3A88%2F");
+        }
+
+        protected void ImageButton2_Click(object sender, ImageClickEventArgs e)
+        {
+            Response.Redirect("sermons.aspx");
+        }
+
+        protected void ImageButton3_Click(object sender, ImageClickEventArgs e)
+        {
+            Response.Redirect("https://tunein.com/radio/Union-Pentecostal-Church-s197173/");
         }
     }
 }
