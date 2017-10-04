6bd085d04f33f4cc4ec94695a8342db9fe04a8e2
Michael <michael@creekviewfarms.net>
2017-10-04 09:34:10 -0400

    Removed Listen debug labels. Started on prayer mechanics

--- upc-website/PrayerReq.aspx
+++ upc-website/PrayerReq.aspx
@@ -10,20 +10,19 @@
 </div>
 <div class="jumbotron text-center">
         
-    <form>
-        <div class="form-group">
-            <label for="email">Email address:</label>
-            <input type="email" class="form-control" id="email">
-        </div>
-        <div class="form-group">
-            <label for="pwd">Password:</label>
-            <input type="password" class="form-control" id="pwd">
-        </div>
-        <div class="checkbox">
-            <label><input type="checkbox"> Remember me</label>
-        </div>
-        <button type="submit" class="btn btn-default">Submit</button>
+    <form id="form1">
+        Name: <asp:TextBox ID="FromName" runat="server" /><br>
+        Would you like to be contacted by a pastor? <br>
+        <asp:RadioButtonList ID="RadioButtonList1" runat="server" DataValueField='Yes, No'></asp:RadioButtonList><br>
+        Email Address: <asp:TextBox ID="FromEmail" runat="server" /><br>
+        
+        Prayer Request: <br />
+        <asp:TextBox ID="PrayerRequest" runat="server" Height="171px" TextMode="MultiLine"  Width="270px" /><br><br>
+        <asp:Button ID="Submit" runat="server" onclick="Submit_Click" Text="Submit" /><br>
+        
     </form>
+
+    <br>
 
     <h4>Other Resources</h4>
     <p><a href="help.aspx">Help</a></p>
--- upc-website/PrayerReq.aspx.cs
+++ upc-website/PrayerReq.aspx.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Net.Mail;
 
 namespace upc_website
 {
@@ -13,5 +14,42 @@ namespace upc_website
         {
 
         }
+
+        protected void Submit_Click(object sender, EventArgs e)
+        {
+
+            System.Net.Mail.MailMessage mail = new System.Net.Mail.MailMessage();
+            mail.To.Add("michael@creekviewfarms.net");
+            mail.From = new MailAddress("upcprayerrequests@gmail.com", "UPC_Prayer_Requests", System.Text.Encoding.UTF8);
+            mail.Subject = "This mail is send from asp.net application";
+            mail.SubjectEncoding = System.Text.Encoding.UTF8;
+            mail.Body = "This is Email Body Text";
+            mail.BodyEncoding = System.Text.Encoding.UTF8;
+            mail.IsBodyHtml = true;
+            mail.Priority = MailPriority.High;
+            SmtpClient client = new SmtpClient();
+            client.Credentials = new System.Net.NetworkCredential("upcprayerrequests@gmail.com", "1101union");
+            client.Port = 587;
+            client.Host = "smtp.gmail.com";
+            client.EnableSsl = true;
+            try
+            {
+                client.Send(mail);
+                //Page.RegisterStartupScript("UserMsg", "<script>alert('Successfully Send...');if(alert){ window.location='SendMail.aspx';}</script>");
+            }
+            catch (Exception ex)
+            {
+                Exception ex2 = ex;
+                string errorMessage = string.Empty;
+                while (ex2 != null)
+                {
+                    errorMessage += ex2.ToString();
+                    ex2 = ex2.InnerException;
+                }
+                //Page.RegisterStartupScript("UserMsg", "<script>alert('Sending Failed...');if(alert){ window.location='SendMail.aspx';}</script>");
+            }
+        }
+
+
     }
 }
