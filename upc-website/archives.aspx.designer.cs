325bba5314a015f35f5ef0f171c3431a36da36bf
HillJD <john.hillen@gmail.com>
2017-10-19 21:57:17 -0400

    Initial foray - Message Archives

--- upc-website/archives.aspx.designer.cs
+++ upc-website/archives.aspx.designer.cs
@@ -13,12 +13,21 @@ namespace upc_website {
     public partial class archives {
         
         /// <summary>
-        /// GVArch control.
+        /// GridView1 control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.GridView GVArch;
+        protected global::System.Web.UI.WebControls.GridView GridView1;
+        
+        /// <summary>
+        /// SqlDataSource1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.SqlDataSource SqlDataSource1;
     }
 }
