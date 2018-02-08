cf005229ad63a68f13dc5f7991a825db367719a5
dearthm <dearthml@gmail.com>
2018-02-08 00:26:17 -0500

    Video changes

---
+++ upc-website/Scripts/js.js
@@ -0,0 +1,22 @@
+﻿$(document).ready(function () {
+    //Play/Pause control clicked
+    var video = $('#videoID');
+    $('.btnPlay').click(function () {
+        if (video[0].paused) {
+            $('button').val('Pause Video');
+            video[0].play();
+        }
+        else {
+            video[0].pause();
+        }
+        return false;
+    });
+
+
+    //var obj = $(".source").parentElement;
+    //alert($(obj).attr('id'));
+    //alert($('video').attr('id'));
+    ////var video = $(this).child
+    //video.play();
+    //});
+})
