c735758afd550b3380b6aafc08f1893e1338af6c
dearthm <dearthml@gmail.com>
2018-02-10 18:19:39 -0500

    added video

---
+++ upc-website/jquery_examples/js.js
@@ -0,0 +1,14 @@
+﻿$(document).ready(function () {
+    //Play/Pause control clicked
+    var whatImage = 2;
+    var video = $('.videoID');
+    $('.btnPlay').on('click',function () {
+        if (video[whatImage].paused) {
+            video[whatImage].play();
+        }
+        else {
+            video[whatImage].pause();
+        }
+        return false;
+    });
+})
