c70d9ab41230cd66b85109b8b541787b2cfd127f
dearthm <dearthml@gmail.com>
2018-02-12 20:42:39 -0500

    changes js.js

--- upc-website/jquery_examples/js.js
+++ upc-website/jquery_examples/js.js
@@ -1,22 +1,33 @@
 ﻿$(document).ready(function () {
+    var v = $(".active").find("video")[0];
+
+    if (v) {
+        alert("true");
+    }
+    else {
+        alert("false");
+    }
+
+
+
     //Play/Pause control clicked
     //var whatImage = 0;
-    video = $('.active').children("video"){
-        "background-color":"red",
-            "border-color": "red",
-                "border-width": "3px",
-                    "border-style": "solid"
-    });;
+    //video = $('.active').children("video"){
+    //    "background-color":"red",
+    //        "border-color": "red",
+    //            "border-width": "3px",
+    //                "border-style": "solid"
+    //});;
 
-    console.log("# Video objects= " + video.length);
+    //console.log("# Video objects= " + video.length);
     
-    function playPause() {
-    if (video.paused) {
-            video.play();
-        }
-        else {
-            video.pause();
-        }
-        return false;
-    };
+    //function playPause() {
+    //if (video.paused) {
+    //        video.play();
+    //    }
+    //    else {
+    //        video.pause();
+    //    }
+    //    return false;
+    //};
 })
