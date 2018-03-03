969c58ad9d0c5747aab2483f2560bbf00dc42a3a
dearthm <dearthml@gmail.com>
2018-03-03 17:24:01 -0500

    deleted default

--- upc-website/Global.asax.cs
+++ upc-website/Global.asax.cs
@@ -14,7 +14,7 @@ namespace upc_website
         void Application_Start(object sender, EventArgs e)
         {
             // Code that runs on application startup
-            RouteConfig.RegisterRoutes(RouteTable.Routes);
+              RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
     }
