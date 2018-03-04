33422a2d6583cb633d93ba364d9751554ff8afde
dearthm <dearthml@gmail.com>
2018-03-03 23:10:42 -0500

    Hopefully correcting global.aspx problems

--- upc-website/jquery_examples/js.js
+++ upc-website/jquery_examples/js.js
@@ -5,7 +5,29 @@
         alert("true");
     }
     else {
-        alert("No video found!");
+        alert("false");
     }
-    v.play();
-}); 
+
+
+
+    //Play/Pause control clicked
+    //var whatImage = 0;
+    //video = $('.active').children("video"){
+    //    "background-color":"red",
+    //        "border-color": "red",
+    //            "border-width": "3px",
+    //                "border-style": "solid"
+    //});;
+
+    //console.log("# Video objects= " + video.length);
+    
+    //function playPause() {
+    //if (video.paused) {
+    //        video.play();
+    //    }
+    //    else {
+    //        video.pause();
+    //    }
+    //    return false;
+    //};
+})
