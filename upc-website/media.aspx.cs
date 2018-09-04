a3ea283c07e89e4d7538dad1ea5943f346787cd9
dearthm <dearthml@gmail.com>
2018-09-04 07:39:23 -0400

    Worked on postback controls for pastordesk

--- upc-website/media.aspx.cs
+++ upc-website/media.aspx.cs
@@ -11,7 +11,28 @@ namespace upc_website
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (IsPostBack)
+            {
+                //if (Request.Form["__EVENTTARGET"] == "Button2")
+                //{
+                string controlName=Request.Params.Get("__eventtarget");
+                Response.Write("Target: " + controlName);
+                label1.Text = controlName;
+                //Response.Write("Argument: " + Request.Form["__EVENTARGUMENT"] + " Target: " + Request.Form["__EVENTTARGET"]);
+                // Fire event
+                //Button2_Click(this, new EventArgs());
+                //}
+            }
+            else
+            {
+                Response.Write("No postBack!");
+                
+            }
+        }
 
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            Response.Write("Mike's Button2 Click Event");
         }
     }
 }
