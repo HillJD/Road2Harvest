98ab301f1b7e29ae994c0265af506b428f5b7c07
HillJD <john.hillen@gmail.com>
2022-09-14 16:43:12 -0400

    Removing unused video page and renaming unused video2 page

--- upc-website/video.aspx.cs
+++ upc-website/video.aspx.cs
@@ -7,7 +7,7 @@ using System.Web.UI.WebControls;
 
 namespace upc_website
 {
-    public partial class video : System.Web.UI.Page
+    public partial class videoLoad : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
